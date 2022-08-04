29a7fd73cc15112d6b0a7e9e5c1c7b2df68bf60d
Lyndon <lyndon.stluce@gmail.com>
2022-08-03 21:04:36 -0400

    commented everything out

--- app.py
+++ app.py
@@ -1,70 +1,70 @@
 from flask import Flask, request, jsonify
-from peewee import *
-from playhouse.shortcuts import model_to_dict, dict_to_model
+#from peewee import *
+#from playhouse.shortcuts import model_to_dict, dict_to_model
+
+#
+#db = PostgresqlDatabase('project', user='postgres',
+#                        password='0000', host='localhost', port=5432)
 
 
-db = PostgresqlDatabase('project', user='postgres',
-                        password='0000', host='localhost', port=5432)
+#class BaseModel(Model):
+ #   class Meta:
+ #       database = db
 
 
-class BaseModel(Model):
-    class Meta:
-        database = db
+#class MilitaryBase(BaseModel):
+ #   base_name = CharField()
+ #   location = CharField()
+ #   branch_in_controll = CharField()
+ #   year_activated = IntegerField()
+ #   miles_from_dmv = IntegerField()
 
 
-class MilitaryBase(BaseModel):
-    base_name = CharField()
-    location = CharField()
-    branch_in_controll = CharField()
-    year_activated = IntegerField()
-    miles_from_dmv = IntegerField()
+#db.connect()
+#db.drop_tables([MilitaryBase])
+#db.create_tables([MilitaryBase])
 
+#MilitaryBase(base_name='Kunsan', location="Gunsan", branch_in_controll="Air Force",
+#             year_activated=1938, miles_from_dmv=140).save()
 
-db.connect()
-db.drop_tables([MilitaryBase])
-db.create_tables([MilitaryBase])
+#MilitaryBase(base_name='Osan', location="Songtan", branch_in_controll="Air Force",#
+#             year_activated=1951, miles_from_dmv=48).save()
 
-MilitaryBase(base_name='Kunsan', location="Gunsan", branch_in_controll="Air Force",
-             year_activated=1938, miles_from_dmv=140).save()
+#MilitaryBase(base_name='Camp Carroll', location="Waegwan", branch_in_controll="Army",
+#             year_activated=1959, miles_from_dmv=134).save()
 
-MilitaryBase(base_name='Osan', location="Songtan", branch_in_controll="Air Force",
-             year_activated=1951, miles_from_dmv=48).save()
+#MilitaryBase(base_name='Camp Castle', location="Dongducheon", branch_in_controll="Army",#
+#             year_activated=1959, miles_from_dmv=20).save()
 
-MilitaryBase(base_name='Camp Carroll', location="Waegwan", branch_in_controll="Army",
-             year_activated=1959, miles_from_dmv=134).save()
+#MilitaryBase(base_name='Camp Eagle', location="Wonju", branch_in_controll="Army",
+#             year_activated=1955, miles_from_dmv=50).save()
 
-MilitaryBase(base_name='Camp Castle', location="Dongducheon", branch_in_controll="Army",
-             year_activated=1959, miles_from_dmv=20).save()
+#MilitaryBase(base_name='Camp Humphreys', location="Dongducheon", branch_in_controll="Army",
+#             year_activated=1950, miles_from_dmv=60).save()
+#
+#MilitaryBase(base_name='Camp Market', location="Yongsan", branch_in_controll="Army",
+#             year_activated=2003, miles_from_dmv=30).save()
 
-MilitaryBase(base_name='Camp Eagle', location="Wonju", branch_in_controll="Army",
-             year_activated=1955, miles_from_dmv=50).save()
+#MilitaryBase(base_name='Camp Red Cloud', location="Uijeongbu", branch_in_controll="Army",
+#             year_activated=1950, miles_from_dmv=20).save()
+#
+#MilitaryBase(base_name='Camp Stanly', location="Uijeongbu", branch_in_controll="Army",
+#             year_activated=1955, miles_from_dmv=15).save()
 
-MilitaryBase(base_name='Camp Humphreys', location="Dongducheon", branch_in_controll="Army",
-             year_activated=1950, miles_from_dmv=60).save()
+#MilitaryBase(base_name='Camp Hovey', location="Dongducheon", branch_in_controll="Army",
+#             year_activated=1952, miles_from_dmv=15).save()
 
-MilitaryBase(base_name='Camp Market', location="Yongsan", branch_in_controll="Army",
-             year_activated=2003, miles_from_dmv=30).save()
+#MilitaryBase(base_name='Camp Casey', location="Dongducheon", branch_in_controll="Army",
+#             year_activated=1952, miles_from_dmv=10).save()
 
-MilitaryBase(base_name='Camp Red Cloud', location="Uijeongbu", branch_in_controll="Army",
-             year_activated=1950, miles_from_dmv=20).save()
+#MilitaryBase(base_name='K 16 Air Base', location="Seongnam", branch_in_controll="Army",
+#             year_activated=1951, miles_from_dmv=35).save()
 
-MilitaryBase(base_name='Camp Stanly', location="Uijeongbu", branch_in_controll="Army",
-             year_activated=1955, miles_from_dmv=15).save()
+#MilitaryBase(base_name='USAG Yongsan', location="Daegu", branch_in_controll="Army",
+#             year_activated=2003, miles_from_dmv=30).save()
 
-MilitaryBase(base_name='Camp Hovey', location="Dongducheon", branch_in_controll="Army",
-             year_activated=1952, miles_from_dmv=15).save()
-
-MilitaryBase(base_name='Camp Casey', location="Dongducheon", branch_in_controll="Army",
-             year_activated=1952, miles_from_dmv=10).save()
-
-MilitaryBase(base_name='K 16 Air Base', location="Seongnam", branch_in_controll="Army",
-             year_activated=1951, miles_from_dmv=35).save()
-
-MilitaryBase(base_name='USAG Yongsan', location="Daegu", branch_in_controll="Army",
-             year_activated=2003, miles_from_dmv=30).save()
-
-MilitaryBase(base_name='USAG Daegu', location="Daegu", branch_in_controll="Army",
-             year_activated=1950, miles_from_dmv=150).save()
+#MilitaryBase(base_name='USAG Daegu', location="Daegu", branch_in_controll="Army",
+#             year_activated=1950, miles_from_dmv=150).save()
 
 app = Flask(__name__)
 
@@ -72,33 +72,33 @@ app = Flask(__name__)
 def index():
   return "US bases in south korea"
 
-@app.route("/militarybase/", methods=['GET', 'POST'])
-@app.route("/<id>", methods=['GET', 'PUT', 'DELETE'])
-def endpoint(id=None):
-    if request.method == 'GET':
-        if id:
-            return jsonify(model_to_dict(MilitaryBase.get(MilitaryBase.id == id)))
-        else:
-            militarybasesList = []
-            for militarybase in MilitaryBase.select():
-                militarybasesList.append(model_to_dict(militarybase))
-            return jsonify(militarybasesList)
+#@app.route("/militarybase/", methods=['GET', 'POST'])
+#@app.route("/<id>", methods=['GET', 'PUT', 'DELETE'])
+#def endpoint(id=None):
+#    if request.method == 'GET':
+#        if id:
+#            return jsonify(model_to_dict(MilitaryBase.get(MilitaryBase.id == id)))
+#        else:
+#            militarybasesList = []
+#            for militarybase in MilitaryBase.select():
+#                militarybasesList.append(model_to_dict(militarybase))
+#            return jsonify(militarybasesList)
 
-    if request.method == 'PUT':
-        body = request.get_json()
-        query = MilitaryBase.update(body).where(MilitaryBase.id == id)
-        query.execute()
-        return jsonify({"updated": True})
+#    if request.method == 'PUT':
+#        body = request.get_json()
+#        query = MilitaryBase.update(body).where(MilitaryBase.id == id)
+#        query.execute()
+#        return jsonify({"updated": True})
 
-    if request.method == 'POST':
-        new_militarybase = dict_to_model(MilitaryBase, request.get_json())
-        new_militarybase.save()
-        return jsonify({"success": True})
+#    if request.method == 'POST':
+#        new_militarybase = dict_to_model(MilitaryBase, request.get_json())
+#        new_militarybase.save()
+#        return jsonify({"success": True})
 
-    if request.method == 'DELETE':
-        deleted = MilitaryBase.delete().where(MilitaryBase.id == id)
-        deleted.execute()
-        return jsonify({"deleted": True})
+#    if request.method == 'DELETE':
+#        deleted = MilitaryBase.delete().where(MilitaryBase.id == id)
+#        deleted.execute()
+#        return jsonify({"deleted": True})
 
 
-app.run(debug=True)
+#app.run(debug=True)
